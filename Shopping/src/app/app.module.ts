1a35ae4939ba9b85881ae83619675bd0b7002ae1
mohammedalbehairy <mohammedalbehairy90@gmail.com>
2019-12-19 10:48:58 +0200

    created home page components

--- Shopping/src/app/app.module.ts
+++ Shopping/src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogComponent } from './blog/blog.component';
+import { ShopComponent } from './shop/shop.component';
+import { FeatureComponent } from './feature/feature.component';
+import { FeatureProductComponent } from './feature-product/feature-product.component';
+import { OfferComponent } from './offer/offer.component';
+import { NewProductComponent } from './new-product/new-product.component';
+import { InspiredProductComponent } from './inspired-product/inspired-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactComponent,
+    BlogComponent,
+    ShopComponent,
+    FeatureComponent,
+    FeatureProductComponent,
+    OfferComponent,
+    NewProductComponent,
+    InspiredProductComponent
   ],
   imports: [
     BrowserModule,
---
+++ Shopping/src/app/inspired-product/inspired-product.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inspired-product',
+  templateUrl: './inspired-product.component.html',
+  styleUrls: ['./inspired-product.component.css']
+})
+export class InspiredProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
